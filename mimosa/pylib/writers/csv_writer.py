ccf44183ec6e25e00bc2e4c83e9da98ae45f0da0
rafelafrance <raphael.lafrance@gmail.com>
2023-01-27 11:40:39 -0500

    Update writers

--- mimosa/pylib/writers/csv_writer.py
+++ mimosa/pylib/writers/csv_writer.py
@@ -1,21 +1,8 @@
-from plants.patterns import term_patterns
 from plants.writers import csv_writer as base_writer
-
-SKIP_FIELD = term_patterns.PARTS + term_patterns.SUBPARTS
-SKIP_FIELD += """ start end trait dimensions taxon """.split()
 
 
 class CsvWriter(base_writer.CsvWriter):
-    @staticmethod
-    def sort_df(df):
-        first = """ taxon """.split()
-
-        rest = sorted(c for c in df.columns if c not in first)
-        columns = first + rest
-
-        df = df[columns]
-
-        return df
+    first = """ taxon """.split()
 
     def format_row(self, row):
         csv_row = {"taxon": row.taxon}
--- mimosa/pylib/writers/html_writer.py
+++ mimosa/pylib/writers/html_writer.py
@@ -1,14 +1,10 @@
 from dataclasses import dataclass
 
-from plants.patterns import term_patterns
 from plants.writers.html_writer import HtmlWriter as BaseWriter
 from plants.writers.html_writer import HtmlWriterRow as BaseWriterRow
 from tqdm import tqdm
 
 from .. import const
-
-TITLE_SKIPS = ["start", "end", "dimensions"]
-TRAIT_SKIPS = TITLE_SKIPS + ["trait"] + term_patterns.PARTS + term_patterns.SUBPARTS
 
 
 @dataclass
