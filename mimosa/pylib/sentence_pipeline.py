fa1078361ee3419c01d224041e2525ef42a58fe4
rafelafrance <raphael.lafrance@gmail.com>
2023-03-03 11:52:04 -0500

    Update tokenizer and sentence parser

--- mimosa/pylib/sentence_pipeline.py
+++ mimosa/pylib/sentence_pipeline.py
@@ -1,7 +1,16 @@
+import regex as re
 from spacy.lang.en import English
-from traiter.pipes.sentence_pipe import SENTENCE
+from traiter.pylib import tokenizer
+from traiter.pylib import tokenizer_util
+from traiter.pylib.pipes.sentence_pipe import SENTENCE
 
-from . import tokenizer
+
+def setup_tokenizer(nlp):
+    not_letter = re.compile(r"[^a-zA-Z.']")
+    removes = [{"pattern": s} for s in nlp.tokenizer.rules if not_letter.search(s)]
+    tokenizer_util.remove_special_case(nlp, removes)
+    tokenizer_util.append_tokenizer_regexes(nlp)
+    tokenizer_util.append_abbrevs(nlp, tokenizer.ABBREVS)
 
 
 def pipeline():
